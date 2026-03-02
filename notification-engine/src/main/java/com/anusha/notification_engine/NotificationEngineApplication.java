6b8789c77cbaaf53a1203d6ea0fd5d09c17eb7f2
Anusha <anushavreddy5@gmail.com>
2026-03-02 12:40:01 +0530

    Fix component scanning for controllers

--- notification-engine/src/main/java/com/anusha/notification_engine/NotificationEngineApplication.java
+++ notification-engine/src/main/java/com/anusha/notification_engine/NotificationEngineApplication.java
@@ -2,12 +2,13 @@ package com.anusha.notification_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.anusha.notification_engine")
 public class NotificationEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationEngineApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationEngineApplication.class, args);
+    }
 }
